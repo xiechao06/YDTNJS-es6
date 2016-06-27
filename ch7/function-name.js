eb6f86929e6ab3cbbcaa60e70d18e080bfa9579f
xiechao06 <xiechao06@gmail.com>
2016-06-27 16:29:25 +0800

    0

---
+++ ch7/function-name.js
@@ -0,0 +1,53 @@
+var assert = require('assert');
+
+assert.equal((function () {}).name, '');
+assert.equal((function *() {}).name, '');
+
+class Awesome {
+    constructor() {}
+    funny() {}
+};
+assert.equal(new Awesome().constructor.name, 'Awesome');
+assert.equal(new Awesome().funny.name, 'funny');
+assert.equal(Awesome.name, 'Awesome');
+
+
+var o = {
+    foo() {},
+    *bar() {},
+    baz: () => {},
+    bam: function () {},
+    ["b" + "iz"]:
+        function() {},
+};
+
+assert.equal(o.foo.name, 'foo');
+assert.equal(o.bar.name, 'bar');
+assert.equal(o.baz.name, '');
+assert.equal(o.bam.name, '');
+assert.equal(o.biz.name, '');
+
+assert.equal(o.foo.bind(o).name, 'bound foo');
+
+class Parent {
+    constructor() {
+        if (new.target === Parent) {
+            this.name = "Parent";
+        }
+        else {
+            this.name = "Child";
+        }
+    }
+}
+
+class Child extends Parent {}
+
+var a = new Parent();
+var b = new Child();
+
+assert.equal(a.name, "Parent");
+assert.equal(b.name, "Child");
+
+
+
+console.log("\n\nDONE...");
---
+++ ch7/well-known-symbols.js
@@ -0,0 +1,4 @@
+var assert = require('assert');
+
+
+console.log("\n\nDONE...");
